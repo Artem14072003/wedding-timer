3ee62699117c1cf0f104bce032c1b3b405cabeb3
Artem14072003 <artem_krivenko_2019@mail.ru>
2026-06-22 10:37:52 +0300

    ayout and animation of images

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@
 
 import "./background/background.js";
 import "./timer/timer.js";
+
+const div = document.querySelector('.timer_container-imgs')
+
+setTimeout(() => div.classList.add('active'), 1000)
